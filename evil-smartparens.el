9fe4eed1c6327197afe6c13bb0771e18908aff00
Lars Andersen <expez@expez.com>
2017-11-03 17:41:55 +0100

    Release 0.4.0

--- evil-smartparens.el
+++ evil-smartparens.el
@@ -5,7 +5,7 @@
 ;; Author: Lars Andersen <expez@expez.com>
 ;; URL: https://www.github.com/expez/evil-smartparens
 ;; Keywords: evil smartparens
-;; Version: 0.3.0
+;; Version: 0.4.0
 ;; Package-Requires: ((evil "1.0") (emacs "24.4") (smartparens "1.10.1"))
 
 ;; This file is not part of GNU Emacs.
